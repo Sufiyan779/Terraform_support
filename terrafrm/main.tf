b68bef8a4ea6ec4a030df969195da9099e4c6190
sufiyan <khansufiyan779@gmail.com>
2022-10-26 15:20:51 +0530

    Added changes

--- terrafrm/main.tf
+++ terrafrm/main.tf
@@ -1,41 +1,10 @@
-resource "aws_vpc" "my_vpc" {
-    cidr_block = "192.168.0.0/16"
-    tags = {
-        Name = "myvpc"
-    }
-
-}
-
-resource "aws_subnet" "first_subnet" {
-    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
-    cidr_block = "192.168.0.0/24"
-
-    tags = {
-      "Name" = "first subnet"
-    }
-
-}
-
-resource "aws_subnet" "second_subnet" {
-    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
-    cidr_block = "192.168.1.0/24"
-
-    tags = {
-      "Name" = "second subnet"
-    }
-
-
-
-}
-
-resource "aws_subnet" "third_subnet" {
-    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
-    cidr_block = "192.168.2.0/24"
-
-    tags = {
-      "Name" = "third subnet"
-    }
-
-
+resource "aws_instance" "terraform" {
+  ami                       = "ami-062df10d14676e201"
+  instance_type             = "t2.micro"
+  key_name                  = "mumbai_region"
+  security_groups           = ["sg-01d71cbe9f383f09f"]
+  tags = {
+    Name                    = "by terraform"
+  }
 
 }
